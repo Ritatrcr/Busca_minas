dbe0759629bbd7277ff6f0d0ad31d20c80e9af82
Rita Trindade da Cruz <ritatrcr@unisabana.edu.co>
2024-05-19 19:15:01 -0500

    Final changes

--- App.py
+++ App.py
@@ -13,18 +13,21 @@ class App(tk.Tk):
         self.title("BuscaMinas (SiPuedes)💣")
         self.resizable(False, False)
         self.geometry("580x580")
-        self.inicio = ImageTk.PhotoImage(file="inicio.jpeg")
-        self.frame = tk.Frame(self, bg="#588a35")
+        self.frame = tk.Frame(self,bg="#588a35")
         self.frame.pack(fill=tk.BOTH, expand=True)
+        try:
+            self.inicio = ImageTk.PhotoImage(file="inicio.jpeg")
+        except Exception as e:
+            print("Error al cargar la imagen:", e)
         self.superior_rectangle = tk.Frame(self.frame, bg="#4a752d", height=75)
         self.superior_rectangle.pack(fill=tk.X)
         self.superior_rectangle.grid_propagate(0)
         self.square = None
+        self.flag_mode=False
         self.grafo = {}
         self.bandera_img_slot = ImageTk.PhotoImage(file="bombaButton.jpeg")
         self.bomba_png = ImageTk.PhotoImage(file="bomba.png")
         self.reloj = ImageTk.PhotoImage(file="reloj.jpeg")
-        self.hover_image = tk.PhotoImage(file="dificil.png")
         self.grid = []
         self.create_difficulty_buttons()
 
@@ -46,50 +49,36 @@ class App(tk.Tk):
 
         self.jugadas_disponibles = 12
         self.bombas_por_encontrar = 32
+        self.flag_mode = False 
 
         self.musica_sin_bomba = pygame.mixer.music.load('intro.mp3')
         pygame.mixer.music.play(1)
 
         welcome_label = tk.Label(self.frame, text="𝕭𝖎𝖊𝖓𝖛𝖊𝖓𝖎𝖉𝖔 𝖆𝖑 𝕭𝖚𝖘𝖈𝖆 𝕸𝖎𝖓𝖆𝖘 💣", bg="#4a752d", fg="white")
-        welcome_label.place(relx=0.5, rely=0.3, anchor=tk.CENTER)
+        welcome_label.pack(pady=100)
 
-        hard_button = ctk.CTkButton(master=self.frame,
+        easy_button = ctk.CTkButton(self.frame,
                                      fg_color=("#92b361", "#4a752d"),
-                                     text="𝔡𝔦𝔣í𝔠𝔦𝔩 😱",
-                                     command=lambda difficulty="Difícl": self.explicacion(difficulty),
+                                     text="¡¡¡EMPEZAR!!!",
+                                     command=lambda difficulty="Fácil": self.explicacion(),
                                      width=200, height=50)
-        hard_button.place(relx=0.5, rely=0.8, anchor=tk.CENTER)
+        easy_button.pack(pady=100)
 
-        medium_button = ctk.CTkButton(master=self.frame,
-                                       fg_color=("#92b361", "#4a752d"),
-                                       text="𝖒𝖊𝖉𝖎𝖔 🫣",
-                                       command=lambda difficulty="Medio ": self.explicacion(difficulty),
-                                       width=200, height=50)
-        medium_button.place(relx=0.5, rely=0.7, anchor=tk.CENTER)
-
-        easy_button = ctk.CTkButton(master=self.frame,
-                                     fg_color=("#92b361", "#4a752d"),
-                                     text="𝔣á𝔠𝔦𝔩 😐",
-                                     command=lambda difficulty="Fácil": self.explicacion(difficulty),
-                                     width=200, height=50)
-        easy_button.place(relx=0.5, rely=0.6, anchor=tk.CENTER)
-
-    def explicacion(self, difficulty):
-        dif = difficulty
+    def explicacion(self):
         self.clean_widgets()
 
-        label = tk.Label(self.frame, text="Si crees que hay una bomba\nhazle click al botón y\nluego a la posición para \nmarcarla y no perder jugadas", borderwidth=0, highlightthickness=0, bg="#4a752d", font="Arial,8")
+        label = tk.Label(self.frame, text="Si crees que hay una bomba hazle click al botón y\nluego a la posición para marcarla y no perder jugadas.\n En el caso de que no haya bombas y hayas seleccionado el botón, perderás 2 jugadas", borderwidth=0, highlightthickness=0, bg="#4a752d", font="Arial,8")
         label.place(relx=0.5, rely=0.4, anchor=tk.CENTER)
 
         listo = ctk.CTkButton(master=self.frame,
                                fg_color=("#92b361", "#4a752d"),
                                text="¿Estás Listo? 🤔",
-                               command=lambda: self.open_grid(dif),
+                               command=lambda: self.open_grid(),
                                width=200, height=50)
 
         listo.place(relx=0.5, rely=0.6, anchor=tk.CENTER)
 
-    def open_grid(self, difficulty):
+    def open_grid(self):
         self.clean_widgets()
         self.informacion_chart()
         self.start_time = time.time()
@@ -115,57 +104,84 @@ class App(tk.Tk):
                 num += 1
 
     def on_click(self, event):
-        widget = event.widget
-        index = self.grid.index(widget)
-        num = index + 1
+        if self.terminar_juego()==False:
+            widget = event.widget
+            index = self.grid.index(widget)
+            num = index + 1
 
-        widget.unbind("<Button-1>")
-        self.jugadas_disponibles -= 1
-        self.label_jugadas_disponibles.config(text=f"Jugadas Restantes: {self.jugadas_disponibles}")
+            widget.unbind("<Button-1>")
+            self.jugadas_disponibles -= 1
+            self.label_jugadas_disponibles.config(text=f"Jugadas Restantes: {self.jugadas_disponibles}")
 
-        if num in self.vertices:
-            random_bg_color = self.random_color()
-            self.label_bomba = tk.Label(self.frame, image=self.bomba_png, borderwidth=0, highlightthickness=0, bg=random_bg_color)
-            self.label_bomba.place(x=widget.winfo_x(), y=widget.winfo_y())
-            widget.config(bg=random_bg_color)
+            if self.flag_mode:  
+                self.flag_mode=False
+                if num in self.vertices:
+                    self.jugadas_disponibles += 1
+                    self.label_jugadas_disponibles.config(text=f"Jugadas Restantes: {self.jugadas_disponibles}")
 
-            self.explotar_bombas(num)
+                    random_bg_color = self.random_color()
+                    self.label_bomba = tk.Label(self.frame, image=self.bomba_png, borderwidth=0, highlightthickness=0, bg=random_bg_color)
+                    self.label_bomba.place(x=widget.winfo_x(), y=widget.winfo_y())
+                    widget.config(bg=random_bg_color)
 
-            self.musica_sin_bomba = pygame.mixer.music.load('Bombas.wav')
-            pygame.mixer.music.play(1)
-            self.bombas_por_encontrar -= 1
-            self.bombas_label.config(text=f"Bombas por encontrar: {self.bombas_por_encontrar}")
-           
-                
+                    self.explotar_bombas(num)
 
-        else:
-            current_color = widget.cget("bg")
-            new_color = "#d7b899" if current_color == "#aad852" else "#e4c29f"
-            widget.config(bg=new_color)
+                    self.musica_sin_bomba = pygame.mixer.music.load('Bombas.wav')
+                    pygame.mixer.music.play(1)
+                    self.bombas_por_encontrar -= 1
+                    self.bombas_label.config(text=f"Bombas por encontrar: {self.bombas_por_encontrar}")
+                else:
+                    current_color = widget.cget("bg")
+                    new_color = "#d7b899" if current_color == "#aad852" else "#e4c29f"
+                    widget.config(bg=new_color)
 
-            self.musica_sin_bomba = pygame.mixer.music.load('Sin-bomba.wav')
-            pygame.mixer.music.play(1)
+                    self.musica_sin_bomba = pygame.mixer.music.load('Sin-bomba.wav')
+                    pygame.mixer.music.play(1)
+                    self.jugadas_disponibles -= 1
+                    self.label_jugadas_disponibles.config(text=f"Jugadas Restantes: {self.jugadas_disponibles}")
+                    popup = tk.Toplevel(self.master)
+                    popup.title("")
+                    popup.geometry("200x100")
+                    popup_label = tk.Label(popup, text="¡¡¡¡NOOOOO!!!!\n Ahí NO habia una bomba😭\nPierdes 2 jugadas")
+                    popup_label.pack(pady=20)
+                    popup.after(3000, popup.destroy)
 
-        self.update()
-        # Esperar 3 segundos antes de limpiar los widgets y mostrar el mensaje
-        
-        if self.terminar_juego():
-            self.clean_widgets()
-            time.sleep(3)
-            restart_button = ctk.CTkButton(master=self.frame,
-                                            fg_color=("#92b361", "#4a752d"),
-                                            text="¿Quieres volver a jugar?",
-                                            command=lambda: self.create_difficulty_buttons(),
-                                            width=200, height=50)
-            restart_button.place(relx=0.5, rely=0.7, anchor=tk.CENTER)
 
-            if self.winner():
-                winner_label = tk.Label(self.frame, text="¡Ganaste! 🎉", font=("Arial", 20), bg="#4a752d", fg="white")
-                winner_label.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+            elif num in self.vertices:
+                random_bg_color = self.random_color()
+                self.label_bomba = tk.Label(self.frame, image=self.bomba_png, borderwidth=0, highlightthickness=0, bg=random_bg_color)
+                self.label_bomba.place(x=widget.winfo_x(), y=widget.winfo_y())
+                widget.config(bg=random_bg_color)
+
+                self.explotar_bombas(num)
+
+                self.musica_sin_bomba = pygame.mixer.music.load('Bombas.wav')
+                pygame.mixer.music.play(1)
+                self.bombas_por_encontrar -= 1
+                self.bombas_label.config(text=f"Bombas por encontrar: {self.bombas_por_encontrar}")
+            
             else:
-                loser_label = tk.Label(self.frame, text="¡Perdiste!  😕", font=("Arial", 20), bg="#4a752d", fg="white")
-                loser_label.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
-            return
+                current_color = widget.cget("bg")
+                new_color = "#d7b899" if current_color == "#aad852" else "#e4c29f"
+                widget.config(bg=new_color)
+
+                self.musica_sin_bomba = pygame.mixer.music.load('Sin-bomba.wav')
+                pygame.mixer.music.play(1)
+
+            self.update()
+            if self.terminar_juego():
+                time.sleep(3)
+                self.mostrar_bombas_no_encontradas()
+                winner_label = tk.Label(self.frame, text=f"Encontraste {32-self.bombas_por_encontrar}/32 bombas", font=("Arial", 20), bg="#588a35", fg="white")
+                winner_label.place(relx=0.5, rely=0.3, anchor=tk.CENTER)
+
+                if self.winner():
+                    winner_label = tk.Label(self.frame, text="¡Ganaste! 🎉", font=("Arial", 20), bg="#588a35", fg="white")
+                    winner_label.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+                else:
+                    loser_label = tk.Label(self.frame, text="¡Perdiste!  😕", font=("Arial", 20), bg="#588a35", fg="white")
+                    loser_label.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+                return
 
     def update_timer(self):
         elapsed_time = time.time() - self.start_time
@@ -176,7 +192,7 @@ class App(tk.Tk):
         self.label_time.after(1000, self.update_timer)
 
     def informacion_chart(self):
-        boton_bandera = tk.Button(self.superior_rectangle, text=" ", image=self.bandera_img_slot, borderwidth=0, highlightthickness=0, command=self.poner_bomba())
+        boton_bandera = tk.Button(self.superior_rectangle, text=" ", image=self.bandera_img_slot, borderwidth=0, highlightthickness=0, command=self.poner_bomba)
         boton_bandera.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
 
         boton_reloj = tk.Label(self.superior_rectangle, text=" ", image=self.reloj, borderwidth=0, highlightthickness=0)
@@ -189,7 +205,7 @@ class App(tk.Tk):
         self.bombas_label.place(relx=0.15, rely=0.3, anchor=tk.CENTER)
 
     def poner_bomba(self):
-        pass
+        self.flag_mode = True
 
     def terminar_juego(self):
         if self.jugadas_disponibles == 0 or self.bombas_por_encontrar == 0:
@@ -246,9 +262,8 @@ class App(tk.Tk):
         print (nodo)
         if nodo in self.grafo:
             bombas_reventadas = 0  # Contador de bombas reventadas
-            if len(self.grafo[nodo]) == 1:  # Si el nodo tiene solo un vecino
-                vecino = next(iter(self.grafo[nodo].keys()))  # Obtener el único vecino del nodo
-                if vecino in self.vertices:
+            for vecino, peso in self.grafo[nodo].items():
+                if peso == 1 and vecino in self.vertices:
                     random_bg_color = self.random_color()
                     # Crear etiqueta con la imagen de la bomba y colocarla en la posición del cuadro vecino
                     label_bomba = tk.Label(self.frame, image=self.bomba_png, borderwidth=0, highlightthickness=0, bg=random_bg_color)
@@ -259,21 +274,11 @@ class App(tk.Tk):
                     pygame.mixer.music.play(1)
                     # Incrementar el contador de bombas reventadas
                     bombas_reventadas += 1
+                    
+                    
                 # Eliminar el vértice vecino del grafo para evitar la propagación de explosiones
                 del self.grafo[vecino]
-            else:  # Si el nodo tiene más de un vecino
-                for vecino, peso in self.grafo[nodo].items():
-                    if vecino in self.vertices:
-                        random_bg_color = self.random_color()
-                        # Crear etiqueta con la imagen de la bomba y colocarla en la posición del cuadro vecino
-                        label_bomba = tk.Label(self.frame, image=self.bomba_png, borderwidth=0, highlightthickness=0, bg=random_bg_color)
-                        label_bomba.place(x=self.grid[vecino - 1].winfo_x(), y=self.grid[vecino - 1].winfo_y())
-                        self.grid[vecino - 1].config(bg=random_bg_color)
-                        # Reproducir sonido de explosión
-                        self.musica_sin_bomba = pygame.mixer.music.load('Bombas.wav')
-                        pygame.mixer.music.play(1)
-                        # Incrementar el contador de bombas reventadas
-                        bombas_reventadas += 1
+                
                 
             # Actualizar el contador de bombas por encontrar restando el número de bombas reventadas
             self.bombas_por_encontrar -= bombas_reventadas
@@ -281,8 +286,16 @@ class App(tk.Tk):
             
             # Eliminar el vértice actual del grafo para evitar la propagación de explosiones
             del self.grafo[nodo]
+            
         else:
             pass
+
+    def mostrar_bombas_no_encontradas(self):
+        for bomba in self.vertices:
+            if bomba in self.grafo:
+                label_bomba = tk.Label(self.frame, image=self.bomba_png, borderwidth=0, highlightthickness=0, bg="black")
+                label_bomba.place(x=self.grid[bomba - 1].winfo_x(), y=self.grid[bomba - 1].winfo_y())
+                self.grid[bomba - 1].config(bg="black")
 
 
 
